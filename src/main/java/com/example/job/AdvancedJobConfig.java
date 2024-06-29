fc70980133e3bede35a06221d627d2c86e3e8116
leejinwoo <leejinwoo1126@gmail.com>
2024-06-29 14:11:42 +0900

    feat: JobExecutionListener 학습

--- src/main/java/com/example/job/AdvancedJobConfig.java
+++ src/main/java/com/example/job/AdvancedJobConfig.java
@@ -2,7 +2,10 @@ package com.example.job;
 
 import com.example.job.validator.LocalDateParameterValidator;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobExecutionListener;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.JobScope;
@@ -29,12 +32,31 @@ public class AdvancedJobConfig {
     }
 
     @Bean
-    public Job advancedJob(Step advanceStep) {
+    public Job advancedJob(Step advanceStep, JobExecutionListener jobExecutionListener) {
         return jobBuilderFactory.get("advancedJob")
                 .incrementer(new RunIdIncrementer())
                 .validator(new LocalDateParameterValidator("targetDate"))
+                .listener(jobExecutionListener)
                 .start(advanceStep)
                 .build();
+    }
+
+    @Bean
+    @JobScope
+    public JobExecutionListener jobExecutionListener() {
+        return new JobExecutionListener() {
+            @Override
+            public void beforeJob(JobExecution jobExecution) {
+                log.info("[jobExecutionListener#beforeJob] jobExecution is " + jobExecution.getStatus());
+            }
+
+            @Override
+            public void afterJob(JobExecution jobExecution) {
+                if(jobExecution.getStatus() == BatchStatus.FAILED) {
+                    log.info("[jobExecutionListener#beforeJob] jobExecution is FAILED");
+                }
+            }
+        };
     }
 
     @Bean
@@ -44,7 +66,8 @@ public class AdvancedJobConfig {
                 .tasklet((contribution, chunkContext) -> {
                     log.info("advanced tasklet executed");
                     log.info("JobParameter targetDate : {}", targetDate);
-                    return RepeatStatus.FINISHED;
+
+                    throw new RuntimeException("ERROR!!!");
                 })
                 .build();
     }
