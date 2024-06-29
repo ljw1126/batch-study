820855f658fd799958f50a02c501613904f6241e
leejinwoo <leejinwoo1126@gmail.com>
2024-06-29 14:55:00 +0900

    feat: StepExecutionListener 학습

--- src/main/java/com/example/job/AdvancedJobConfig.java
+++ src/main/java/com/example/job/AdvancedJobConfig.java
@@ -3,13 +3,17 @@ package com.example.job;
 import com.example.job.validator.LocalDateParameterValidator;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.BatchStatus;
+import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobExecutionListener;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.core.StepExecutionListener;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Value;
@@ -61,14 +65,32 @@ public class AdvancedJobConfig {
 
     @Bean
     @JobScope
-    public Step advanceStep(@Value("#{jobParameters['targetDate']}") String targetDate) {
+    public Step advanceStep(@Value("#{jobParameters['targetDate']}") String targetDate, StepExecutionListener stepExecutionListener) {
         return stepBuilderFactory.get("advancedStep")
+                .listener(stepExecutionListener)
                 .tasklet((contribution, chunkContext) -> {
                     log.info("advanced tasklet executed");
                     log.info("JobParameter targetDate : {}", targetDate);
 
-                    throw new RuntimeException("ERROR!!!");
+                    return RepeatStatus.FINISHED;
                 })
                 .build();
+    }
+
+    @Bean
+    @StepScope
+    public StepExecutionListener stepExecutionListener() {
+        return new StepExecutionListener() {
+            @Override
+            public void beforeStep(StepExecution stepExecution) {
+                log.info("[StepExecutionListener#beforeStep] stepExecution is " + stepExecution.getStatus());
+            }
+
+            @Override
+            public ExitStatus afterStep(StepExecution stepExecution) {
+                log.info("[StepExecutionListener#afterStep] stepExecution is " + stepExecution.getStatus());
+                return stepExecution.getExitStatus();
+            }
+        };
     }
 }
